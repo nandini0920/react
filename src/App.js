d3015cbb39bdb1aec6b6db50e481afc9f7bb6f88
nandini0920 <nandini0920.be21@chitkara.edu.in>
2022-06-09 18:51:22 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Todo from "./components/Project/list";
+// import Temp from "./components/weather/temp";
+
+const App = () => {
+  return (
+    <>
+      <Todo />
+    </>
+  );
+};
+
+export default App;
